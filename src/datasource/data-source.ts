aaf475eb6c3b44dc06f414cf84411382554d86f3
CLTNWorkspace <claiton.t@protonmail.com>
2022-08-28 17:25:52 -0300

    ajuste env

--- src/datasource/data-source.ts
+++ src/datasource/data-source.ts
@@ -1,19 +1,24 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Agendamento } from "./entity/Agendamento";
+import { CreateAgendamento1661482432672 } from "./migration/1661482432672-CreateAgendamento";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.APP_DATABASE,
   synchronize: true,
   logging: false,
   entities: [Agendamento],
-  migrations: ['src/datasource/migration/'],
+  migrations: [CreateAgendamento1661482432672],
   subscribers: [],
 });
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("Database conectada");
+    console.log("Database conectaada");
   })
   .catch((error) => console.log(error));
